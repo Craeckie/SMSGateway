e95199823f903e6c5675229593061f301dbec368
Craeckie <josia-internet@rosskopfs.de>
2018-06-06 17:05:00 +0200

    TG: try to match using display_name

--- smsgateway/sources/commands/send_telegram_new.py
+++ smsgateway/sources/commands/send_telegram_new.py
@@ -24,21 +24,35 @@ def run(lines):
     toL = lines[1]
     m = re.match("To: (.*)$", toL)
     if m:
-      to = m.group(1).replace(' ', '')
-      print("Matched To: %s" % to)
+      to_matched = m.group(1).replace(' ', '')
+      print("Matched To: %s" % to_matched)
+      to = None
+      for x in client.iter_dialogs():
+          name = utils.get_display_name(x.entity)
+          if name == to_matched:
+            to = name.entity.id
+            print("Found it via display_name: %s" % x.entity.stringify())
+            break
+      if not to:
+          print(f"Couldn't find {to}! Trying directly..")
+          to = name = to_matched
       msg = '\n'.join(lines[2:])
       print("Sending Telegram msg:\n%s" % msg)
-      arg = "msg %s %s" % (to, msg)
+      # arg = "msg %s %s" % (to, msg)
       # print("Calling Telegram with %s" % arg)
       import getpass
       print("I am: %s" % getpass.getuser())
 
       client = TelegramClient(session_path, api_id, api_hash)
-      if not client.start():
-          app_log.error("Could not connect! Run python3 -m smsgateway.source.commands.send_telegram_new to authorize!")
-          sys.exit(1)
+      try:
+          client.start()
+      except Exception as e:
+          ret = "Could not connect! Run python3 -m smsgateway.sources.commands.send_telegram_new to authorize!\nError: %s" % e
+          app_log.error(ret)
+          return ret
       client.send_message(to, msg)
       client.disconnect()
+      ret = f"TG\nTo: {name}\n{msg}"
       # try:
       #     #res = subprocess.check_output(["sudo", "-Hu", "pi", "/home/pi/tg/bin/telegram-cli", "-D", "--json", "-RW", "-e", arg], stderr=subprocess.STDOUT).decode('UTF-8').strip()
       #     res = subprocess.check_output([TELEGRAM_CLI_PATH, "-D", "--json", "-k", TELEGRAM_KEY_PATH, "--profile", "profile_send", "-RW", "-e", arg], stderr=subprocess.STDOUT, timeout=300).decode('UTF-8').strip()
--- smsgateway/sources/telegram_new.py
+++ smsgateway/sources/telegram_new.py
@@ -16,7 +16,7 @@ api_hash = "80cbc97ce425aae38c1e0291ef2ab2a4"
 session_path = os.path.join(CONFIG_DIR, 'telegram')
 client = TelegramClient(session_path, api_id, api_hash,update_workers=1, spawn_read_thread=False)
 if not client.start():
-    app_log.error("Could not connect! Run python3 -m smsgateway.source.telegram to authorize!")
+    app_log.error("Could not connect! Run python3 -m smsgateway.sources.telegram to authorize!")
     sys.exit(1)
 
 app_log.info("Started TelegramClient")
