d2c082ca4e6b12351d5f7f86f2dd5257c88ced21
Craeckie <josia-internet@rosskopfs.de>
2018-06-18 17:43:22 +0200

    Added support for media(geo, photo, contact) and forwarded from; Workaround for catch_up

--- smsgateway/sources/telegram_new.py
+++ smsgateway/sources/telegram_new.py
@@ -1,10 +1,10 @@
-import sys, os
+import sys, os, signal
 from smsgateway import sink_sms
 from smsgateway.sources.utils import *
 from smsgateway.config import *
 
 from telethon import TelegramClient, events
-from telethon.tl.types import Chat, User
+from telethon.tl.types import Chat, User, MessageMediaGeo, MessageMediaContact, MessageMediaPhoto
 from telethon.tl.functions.users import GetFullUserRequest
 
 app_log = setup_logging("telegram")
@@ -22,6 +22,18 @@ if not client.start():
 
 app_log.info("Started TelegramClient")
 
+def get_user_name(user_id):
+    user_name = ""
+    user_entity = client.get_entity(user_id)
+    if isinstance(user_entity, User):
+      #user = client(GetFullUserRequest(user_id)).user
+
+      user_number = user_entity.phone if user_entity.phone else ""
+      user_name = ' '.join([x for x in [user_entity.first_name, user_entity.last_name] if x])
+      if user_number:
+          user_name += " (%s)" % user_number
+    return user_name
+
 @client.on(events.NewMessage())
 def callback(event):
 
@@ -31,15 +43,7 @@ def callback(event):
       else:
         user_id = event.message.from_id
 
-      user_name = ""
-      user_entity = client.get_entity(user_id)
-      if isinstance(user_entity, User):
-        #user = client(GetFullUserRequest(user_id)).user
-
-        user_number = user_entity.phone if user_entity.phone else ""
-        user_name = ' '.join([x for x in [user_entity.first_name, user_entity.last_name] if x])
-        if user_number:
-            user_name += " (%s)" % user_number
+      user_name = get_user_name(user_id)
 
       group_entity = client.get_entity(event.message.to_id)
       if isinstance(group_entity, Chat):
@@ -49,7 +53,44 @@ def callback(event):
           else:
               user_name += f"@{group_name}"
 
-      msg = event.message.message
+      msg = ""
+
+      if event.message.fwd_from:
+          from_id = event.message.from_id
+          from_name = get_user_name(from_id)
+          msg += f"Forwarded from {from_name}:\n"
+
+      # if event.message.reply_to_msg_id:
+      #     for message in client.iter_messages('username', limit=10):
+      msg += event.message.message
+      media = event.message.media
+      if media:
+          if msg:
+              msg += "\n"
+          if isinstance(media, MessageMediaGeo):
+              geo = media.geo
+              msg += f"https://osmand.net/go?lat={geo.lat}&lon={geo.long}&z=15"
+          elif isinstance(media, MessageMediaPhoto):
+              photo = media.photo
+              msg += "Media: Photo"
+              if photo.sizes:
+                  largest = [s for s in photo.sizes if s.type == 'y']
+                  if len(largest) >= 1:
+                      largest = largest[0]
+                      msg += f" ({largest.w}x{largest.h})"
+          elif isinstance(media, MessageMediaContact):
+              msg += "Media: Contact\n"
+              if media.first_name or media.last_name:
+                  msg += f"Name:"
+                  if media.first_name:
+                      msg += f" {media.first_name}"
+                  if media.last_name:
+                      msg += f" {media.last_name}"
+                  msg += "\n"
+              if media.phone_number:
+                  msg += f"Phone: {media.phone_number}"
+          else:
+              msg += "Media: Unknown"
       if event.message.out:
           app_log.info("New message to %s!" % user_name)
           sink_sms.send_from_me(IDENTIFIER, user_name, msg)
@@ -59,6 +100,20 @@ def callback(event):
       app_log.debug(msg)
     except Exception as e:
         app_log.warning(e)
+
+app_log.info("Catching up..")
+
+def handler(signum, frame):
+    if signum == signal.SIGALRM:
+      app_log.info("Got a signal for timeout, raising exception")
+      raise Exception("Function timed out!")
+signal.signal(signal.SIGALRM, handler)
+signal.alarm(10)
+try:
+    client.catch_up()
+except Exception as e:
+    app_log.warning("client.catch_up failed with Exception: %s" % e)
+signal.alarm(0)
 
 app_log.info("Listening to messages..")
 
