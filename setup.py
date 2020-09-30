69cbd1fcfca7b59bf6056f87f38980cb26b7756c
Craeckie <josia-internet@rosskopfs.de>
2020-09-30 18:44:14 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='SMSGateway',
-    version='0.7.0.4',
+    version='0.7.0.5',
     url="https://github.com/Craeckie/SMSGateway",
     description="Forwards messages via SMS from and to instant messengers",
     packages=setuptools.find_packages(),
