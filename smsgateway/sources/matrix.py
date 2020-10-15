2b1053289cc7a7bcf79561cb4d310a29568f8eaa
craeckie <josia-internet@rosskopfs.de>
2020-10-15 21:05:57 +0200

    Matrix: currently handle only text (and images)

--- smsgateway/sources/matrix.py
+++ smsgateway/sources/matrix.py
@@ -15,54 +15,57 @@ app_log = setup_logging("matrix")
 async def message_callback(room: MatrixRoom, event: Event) -> None:
     if not isinstance(event, RoomMessageText):
         print(event)
+    if type == 'm.room.encrypted' and event.decrypted == False:
+        return
 
-    app_log.info(
-        f"Message received in room {room.display_name}\n"
-        f"{room.user_name(event.sender)} | {event.body}"
-    )
-    is_group = len(room.users.keys()) > 2 or room.is_named
+    if isinstance(event, RoomMessageText) or isinstance(event, RoomMessageImage):
+        app_log.info(
+            f"Message received in room {room.display_name}\n"
+            f"{room.user_name(event.sender)} | {event.body}"
+        )
+        is_group = len(room.users.keys()) > 2 or room.is_named
 
-    if is_group and not room.is_named:
-        room_name = ', '.join([user.display_name for id, user in room.users.items() if id != room.own_user_id])
-    else:
-        room_name = room.display_name
-
-    chat_info = {
-        'ID': event.event_id,
-        'date': datetime.utcfromtimestamp(event.server_timestamp / 1000),
-        'type': 'Group' if is_group else 'User'
-    }
-    if event.sender == room.own_user_id:  # I sent this message
-        if is_group:
-            chat_info.update({
-                'to_id': room.room_id,
-                'to': room_name
-            })
+        if is_group and not room.is_named:
+            room_name = ', '.join([user.display_name for id, user in room.users.items() if id != room.own_user_id])
         else:
-            to_user = [user for u_id, user in room.users.items() if u_id != event.sender][0]
-            chat_info.update({
-                'to_id': to_user.user_id,
-                'to': to_user.display_name
-            })
+            room_name = room.display_name
 
-    else:
-        from_user = [user for u_id, user in room.users.items() if u_id == event.sender][0]
-        chat_info.update({
-            'from_id': from_user.user_id,
-            'from': from_user.display_name
-        })
-        if is_group:
-            chat_info.update({
-                'to_id': room.room_id,
-                'to': room_name
-            })
+        chat_info = {
+            'ID': event.event_id,
+            'date': datetime.utcfromtimestamp(event.server_timestamp / 1000),
+            'type': 'Group' if is_group else 'User'
+        }
+        if event.sender == room.own_user_id:  # I sent this message
+            if is_group:
+                chat_info.update({
+                    'to_id': room.room_id,
+                    'to': room_name
+                })
+            else:
+                to_user = [user for u_id, user in room.users.items() if u_id != event.sender][0]
+                chat_info.update({
+                    'to_id': to_user.user_id,
+                    'to': to_user.display_name
+                })
 
-    text = event.body
-    if isinstance(event, RoomMessageImage):
-        text += ' (Picture)'
-    #if event.
-    #chat_info['edit']
-    sink_sms.send_dict(IDENTIFIER, event.body, chat_info)
+        else:
+            from_user = [user for u_id, user in room.users.items() if u_id == event.sender][0]
+            chat_info.update({
+                'from_id': from_user.user_id,
+                'from': from_user.display_name
+            })
+            if is_group:
+                chat_info.update({
+                    'to_id': room.room_id,
+                    'to': room_name
+                })
+
+        text = event.body
+        if isinstance(event, RoomMessageImage):
+            text += ' (Picture)'
+        #if event.
+        #chat_info['edit']
+        sink_sms.send_dict(IDENTIFIER, event.body, chat_info)
 
 
 async def main() -> None:
